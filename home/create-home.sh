904c6018817adfa94d1725622258110a76227704
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2022-11-30 11:44:38 -0600

    IPv6 nodes.  Central does not yet do IPv6.

--- home/create-home.sh
+++ home/create-home.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 # `envsubst` replaces envrionment variables with their values.
-export PROJECT=nautilus-tutorial
+: ${REGION:=us-central}
+: ${STORAGE_REGION:=-central} # '-central', '-east', or '' for west.
+PROJECT=nautilus-tutorial
+export PROJECT REGION
+echo "=== create-home.sh $PROJECT $REGION"
+
+echo "+++ Create Pod"
 cat container/home-template.yaml | envsubst | kubectl apply -f -
 
 while [ $(kubectl get pod/home -o template --template={{.status.phase}}) != "Running" ] ; do
     kubectl describe pod home
-    echo "Waiting for pod/home: $(date)"
+    echo "+++ Waiting for pod/home: $(date)"
     sleep 5
 done
 
-echo "Setup PV"
+echo "+++ Setup PV"
 ./ssh.sh 'sudo chown -v $USER: /home/$USER /scratch'
