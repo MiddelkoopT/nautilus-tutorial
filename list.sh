eb98509ef8405f7b2a491f8f5c60418228a0697a
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2023-10-23 15:38:20 -0500

    Add virtualmachine listing

--- list.sh
+++ list.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+kubectl get secrets
 kubectl get persistentvolumeclaims
 kubectl get deployment
 kubectl get statefulset
 kubectl get ingress
 kubectl get service
+kubectl get virtualmachine
 kubectl get pods
-kubectl get secrets
